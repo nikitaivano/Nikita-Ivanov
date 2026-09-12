95ca09223f2b6c1c916dda9b3ed11fff3a62fc9b
Nikita Ivanov <nikitaivanov@kpfu.ru>
2026-09-12 15:42:13 +0300

    Initial commit

---
+++ Home/Home1/4.cs
@@ -0,0 +1,5 @@
+int a = 10;
+int b = 20;
+int sum;
+sum = a + b;
+Console.WriteLine(sum);
---
+++ Home/Home1/4mini-prolect.cs
@@ -0,0 +1,6 @@
+int chislo;
+chislo=int.Parse(Console.ReadLine());
+if (chislo == 7)
+	Console.WriteLine("Ты угадал");
+else
+	Console.WriteLine("Ты не угадал");
